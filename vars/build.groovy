7b31f8356f13223972741d16f956c20380e5278f
廖柏仰 <gearsinsky@gmail.com>
2025-05-24 10:23:18 +0800

    update groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,4 +1,4 @@
-def call() {
+def call(String projectname = 'python-project') {
     echo "Building image: ${env.PROJECT_NAME}"
     sh """
         docker build --platform linux/amd64 -t ${env.PROJECT_NAME}:${BUILD_NUMBER} .
--- vars/dockerpush.groovy
+++ vars/dockerpush.groovy
@@ -1,15 +1,18 @@
-def call() {
+def call(String projectname = 'python-project', String dockerRepo = 'owen0522/jenkins_dind') {
     echo "Pushing image to DockerHub"
-    echo "Deploying tag ${env.GIT_TAG_NAME}"
+    echo "Deploying tag: ${env.GIT_TAG_NAME ?: 'no-tag'}"
 
-    withCredentials([usernamePassword(credentialsId: 'docker-repo', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASWD')]) {
+    withCredentials([usernamePassword(
+        credentialsId: 'docker-repo',
+        usernameVariable: 'DOCKER_USER',
+        passwordVariable: 'DOCKER_PASWD'
+    )]) {
         sh """
-            echo "$DOCKER_PASWD" | docker login -u "$DOCKER_USER" --password-stdin
-            docker tag ${PROJECT_NAME}:${BUILD_NUMBER} ${DOCKER_REPO}:${BUILD_NUMBER}
-            docker tag ${PROJECT_NAME}:${BUILD_NUMBER} ${DOCKER_REPO}:latest
-            docker push ${DOCKER_REPO}:${BUILD_NUMBER}
-            docker push ${DOCKER_REPO}:latest
+            echo "\$DOCKER_PASWD" | docker login -u "\$DOCKER_USER" --password-stdin
+            docker tag ${projectname}:${env.BUILD_NUMBER} ${dockerRepo}:${env.BUILD_NUMBER}
+            docker tag ${projectname}:${env.BUILD_NUMBER} ${dockerRepo}:latest
+            docker push ${dockerRepo}:${env.BUILD_NUMBER}
+            docker push ${dockerRepo}:latest
         """
     }
 }
-
