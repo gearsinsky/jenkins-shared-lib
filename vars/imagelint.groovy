8e29832ac939acd208b22c0e78a849ea0aec12e6
廖柏仰 <gearsinsky@gmail.com>
2025-05-24 16:54:09 +0800

    fix imagelint

--- vars/imagelint.groovy
+++ vars/imagelint.groovy
@@ -4,7 +4,6 @@ def call() {
         export DOCKER_HOST=tcp://docker-dind-daemon:2375
         echo $DOCKER_HOST
         docker run --rm \
-        --network jenkins-net \
         --add-host=docker-dind-daemon:172.18.0.5 \
         aquasec/trivy image --docker-host tcp://docker-dind-daemon:2375 ${env.PROJECT_NAME}:${BUILD_NUMBER}
     """
